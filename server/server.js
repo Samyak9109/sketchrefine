af0af6f44d79620a35266f8bf81da97032151761
Samyak Jain <samyak.9109@gmail.com>
2026-06-18 16:54:27 +0530

    crating socket-io server, setting backend

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("client connected:", socket.id);
+});
+
+server.listen(3001, () => {
+  console.log("server running on 3001");
+});
